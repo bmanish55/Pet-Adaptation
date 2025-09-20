248d8369b41e3615e95a70321510817ffb305ac9
bmanish55 <mbhavar143@gmail.com>
2025-09-21 01:10:32 +0530

    Initial commit

--- partials-front/menu.php
+++ partials-front/menu.php
@@ -10,7 +10,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Pet Adaptation & Vet Community</title>
+  <title>Adoptly: Pet Adoption</title>
   <link rel="stylesheet" href="css/style.css" />
   <link rel="icon" href="images/favicon-2.png" />
 </head>
